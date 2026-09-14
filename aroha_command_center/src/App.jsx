e0f4d352db0ac88e7ed7c1acea74a99c3c6806a3
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2026-09-14 19:08:00 +0530

    refactor: extract web application routes

--- aroha_command_center/src/App.jsx
+++ aroha_command_center/src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import CommandCenterHome from './pages/CommandCenterHome';
-import InventoryRisk from './pages/InventoryRisk';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 import './App.css';
-import CargoResupply from './pages/CargoResupply';
-import ExpeditionsPersonnel from './pages/ExpeditionsPersonnel';
-import EmergencyMonitor from './pages/EmergencyMonitor';
-import ForecastRecalibration from './pages/ForecastRecalibration';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Active Routes */}
-        <Route path="/" element={<CommandCenterHome />} />
-        <Route path="/inventory" element={<InventoryRisk />} />
-        <Route path="/cargo" element={<CargoResupply/>} />
-        <Route path="/expeditions" element={<ExpeditionsPersonnel />} />
-        <Route path="/alerts" element={<EmergencyMonitor />} />
-        <Route path="/settings" element={<ForecastRecalibration />} />
-        
-        {/* Fallback routing */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
